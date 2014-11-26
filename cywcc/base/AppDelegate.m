c9a1c303adbebc08ca13908e53622de71ae30674
wangdehuai <wangdehuai@me.com>
2014-11-26 10:26:25 +0800

    appdelegate 中文注释

--- cywcc/base/AppDelegate.m
+++ cywcc/base/AppDelegate.m
@@ -9,13 +9,11 @@
 #import "AppDelegate.h"
 #import "TabViewController.h"
 
-
 @interface AppDelegate ()
 
 @end
 
 @implementation AppDelegate
-
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
@@ -27,25 +25,25 @@
 }
 
 - (void)applicationWillResignActive:(UIApplication *)application {
-    // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-    // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+    //当应用程序即将从活动转移到非活动状态时发送。这可能会发生某些类型的临时中断（例如呼入电话呼叫或SMS消息），或者当用户退出该应用程序和它开始过渡到背景状态。
+    //使用这个方法来暂停正在进行的工作，关闭计时器，并踩下油门的OpenGL ES的帧速率。游戏应该使用这种方法来暂停游戏。
 }
 
 - (void)applicationDidEnterBackground:(UIApplication *)application {
-    // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-    // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+    //使用这个方法来释放共享资源，保存用户数据，无效计时器，并储存足够的应用程序状态信息到你的应用程序恢复到其当前状态的情况下，它后来终止。
+    //如果您的应用程序支持后台运行，这种方法被称为代替applicationWillTerminate：当用户退出。
 }
 
 - (void)applicationWillEnterForeground:(UIApplication *)application {
-    // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
+    //调用作为从背景到非活动状态的转换的一部分;在这里您可以撤消许多就进入后台的变化。
 }
 
 - (void)applicationDidBecomeActive:(UIApplication *)application {
-    // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+    //重新启动被暂停的（或尚未开始），而应用程序是不活动的任何任务。如果该应用程序是以前的背景，任选刷新用户界面。
 }
 
 - (void)applicationWillTerminate:(UIApplication *)application {
-    // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+    //当应用程序即将终止调用。如果适当保存数据。另请参见applicationDidEnterBackground:.
 }
 
 @end
